0a3bb444746684ba205344e1f30b7cd3081a516c
Jessica <jessicabyrne812@gmail.com>
2021-02-16 19:06:34 +0000

    Initial Commit

---
+++ Main Files/index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const checkForReactions = require('./checkForReactions');
+const client = new Discord.Client()
+const config = require('./config.json');
+
+client.on('ready', () => {
+    client.channels.cache.get('809476477321609306').messages.fetch({limmit: 1})
+        .then(messages => {
+            var reactionMessage = messages.first();
+            reactionMessage.reactions.removeAll();
+        });
+})
+
+client.on('ready', () => {
+    console.log('Client is ready')
+    checkForReactions(client)
+})
+
+client.login(config.discordAuthToken)
