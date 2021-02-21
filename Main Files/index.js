ac28043f0e72f73ba2287506d1cf2c47c611bc03
Jessica <jessicabyrne812@gmail.com>
2021-02-21 21:31:15 +0000

    dumb folder

--- Main Files/index.js
+++ Main Files/index.js
@@ -2,10 +2,11 @@ const Discord = require('discord.js');
 const checkForReactions = require('./checkForReactions');
 const client = new Discord.Client()
 require('dotenv').config()
+const discordAuthToken = process.env.discordAuthToken;
 
 client.on('ready', () => {
     console.log('Client is ready')
     checkForReactions(client)
 })
 
-client.login(process.env.discordAuthToken)
+client.login(discordAuthToken)
