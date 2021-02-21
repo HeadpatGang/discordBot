ce40a703cca31f5cc68f55adeb534d927b868291
Jessica <jessicabyrne812@gmail.com>
2021-02-21 02:23:49 +0000

    Removing all reactions when bot loads is kinda dumb so I removed it.

--- Main Files/index.js
+++ Main Files/index.js
@@ -4,15 +4,6 @@ const client = new Discord.Client()
 const config = require('./config.json');
 
 client.on('ready', () => {
-    console.log("Client is online\n" + "Client is now removing reactions.")
-    client.channels.cache.get('812490492700393524').messages.fetch({limmit: 1})
-        .then(messages => {
-            var reactionMessage = messages.first();
-            reactionMessage.reactions.removeAll();
-        });
-})
-
-client.on('ready', () => {
     console.log('Client is ready')
     checkForReactions(client)
 })
