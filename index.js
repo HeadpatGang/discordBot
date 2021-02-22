eb86266d8f7320801672fa3e35ef714a7e12cd26
Jessica <jessicabyrne812@gmail.com>
2021-02-22 15:38:33 +0000

    test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const checkForReactions = require('./checkForReactions');
 const client = new Discord.Client()
-require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
 const discordAuthToken = process.env.discordAuthToken;
 
 client.on('ready', () => {
