ba81e32f9275dc39fcdcc83272732c6a06cd1cc2
Jessica <jessicabyrne812@gmail.com>
2021-02-26 15:07:50 +0000

    Bots are weird, just doing needless pushes for heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const checkForReactions = require('./checkForReactions');
 const client = new Discord.Client()
+//require('dotenv').config();
 const discordAuthTokenBDB = process.env.discordAuthTokenBDB;
 const discordAuthTokenSSB = process.env.discordAuthTokenSSB
 
