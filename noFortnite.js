127c8746dd66a054b52b457df62499a54371ac89
Jessica <jessicabyrne812@gmail.com>
2021-03-01 02:19:30 +0000

    LOL

--- noFortnite.js
+++ noFortnite.js
@@ -1,5 +1,8 @@
 module.exports = client => {
-    if(message.content.includes("fortnite") && message.author.id === '213346955131092993') {
-        message.react('814559639496425502')
-    }
+    client.on('message', message => {
+        if(message.content.includes("fortnite") && message.author.id === '213346955131092993') {
+            message.react('814559639496425502')
+        }
+    })
+    
 }
