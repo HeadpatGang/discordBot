43f9515b5a6e0de9050729502d8a1c9fe6f41cbb
Jessica <jessicabyrne812@gmail.com>
2021-03-01 02:18:04 +0000

    I'm not even gonna test this, I just know it works

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const checkForReactions = require('./checkForReactions');
 const checkForModIdea = require('./checkForModIdea');
+const noFortnite = require('./noFortnite')
 const client = new Discord.Client()
 //require('dotenv').config();
 const discordAuthTokenBDB = process.env.discordAuthTokenBDB;
@@ -10,6 +11,7 @@ client.on('ready', () => {
     console.log('Client is ready')
     checkForReactions(client)
     checkForModIdea(client)
+    noFortnite(client)
     //commandHandler(client)
 })
 
