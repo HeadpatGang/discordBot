e8a7caa1b9c050f6a2d3a943bdc7cdc903895e4f
Jessica <jessicabyrne812@gmail.com>
2021-03-01 02:24:21 +0000

    Who needs testing in the year 2021

--- noFortnite.js
+++ noFortnite.js
@@ -1,6 +1,6 @@
 module.exports = client => {
     client.on('message', message => {
-        if(message.content.includes("fortnite") && message.author.id === '213346955131092993') {
+        if(message.content.toLowerCase().includes("fortnite")&& message.author.id === '213346955131092993') {
             message.react('814559639496425502')
         }
     })
