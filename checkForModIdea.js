c787b22e3a7123b4aa8eb715c1a04e1b84e1d8ab
Jessica <jessicabyrne812@gmail.com>
2021-03-07 01:22:53 +0000

    oops

--- checkForModIdea.js
+++ checkForModIdea.js
@@ -2,8 +2,6 @@ const channelID = ['807377739007000658']
 
 module.exports = client => {
     client.on('message', message => {
-        console.log(message.channel.id)
-        console.log(channelID[0])
         if(!message.author.bot) return;
         if(message.channel.id === channelID[0]) {
             message.react("👍")
